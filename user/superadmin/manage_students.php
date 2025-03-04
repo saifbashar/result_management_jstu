9f0bba8ce135fc6846ec8e1f3cbe9a72573cd272
As-Rahman <asrahman111@gmail.com>
2025-03-05 04:50:31 +0600

    student section added

--- user/superadmin/manage_students.php
+++ user/superadmin/manage_students.php
@@ -357,20 +357,22 @@ try {
             <footer class="py-4 bg-light mt-auto">
                 <div class="container-fluid px-4">
                     <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright © Your Website 2023</div>
+                        <div class="text-muted">Copyright © ASA</div>
                         <div>
                             <a href="#">Privacy Policy</a>
-                            · <a href="#">Terms & Conditions</a>
+                            · <a href="#">Terms & Conditions</a> 
                         </div>
                     </div>
                 </div>
             </footer>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        crossorigin="anonymous"></script>
     <script src="js/scripts.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
-    <script src="js/datatables-simple-demo.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"
+        crossorigin="anonymous"></script>
+    <!-- <script src="js/datatables-simple-demo.js"></script>
     <script>
         document.addEventListener('DOMContentLoaded', () => {
             const dataTable = new simpleDatatables.DataTable("#datatablesSimple", {
